d101fc7d199b49e147063d21bf9c6faacea702b5
RG <371662174@qq.com>
2021-05-26 08:58:01 +0800

    0.2

--- src/main/java/com/home/git/GitTest.java
+++ src/main/java/com/home/git/GitTest.java
@@ -7,5 +7,6 @@ package com.home.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello java！");
+        System.out.println("Hello World！");
     }
 }
