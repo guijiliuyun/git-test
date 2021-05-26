4bc1fffb4352d58e4a61225ecbeb5c835e843bd2
RG <371662174@qq.com>
2021-05-26 09:13:39 +0800

    0.4

--- src/main/java/com/home/git/GitTest.java
+++ src/main/java/com/home/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("Hello java！");
         System.out.println("Hello World！");
         System.out.println("Hello Dream！");
-        System.out.println("Hello Dream！");
+        System.out.println("Hello PHP！");
+        System.out.println("Hello C！");
     }
 }
